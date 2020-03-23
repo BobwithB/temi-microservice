7dfb2caf6733cd85a629728ddb5da067b9f0ef70
Bob Wei <bob.wei@robotemi.com>
2020-03-23 12:39:11 +0800

    upate new code
    Signed-off-by: Bob Wei <bob.wei@robotemi.com>

---
+++ src/main/java/com/example/demo/ApplicationRunnerImpl.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationRunnerImpl implements ApplicationRunner {
+
+    FaceEngineService faceEngineService = new FaceEngineService();
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        System.out.println("通过实现ApplicationRunner接口，在spring boot项目启动后打印参数");
+        System.out.println("通过实现ApplicationRunner接口，在spring boot项目启动后打印参数");
+        faceEngineService.startFaceEngine();
+        String[] sourceArgs = args.getSourceArgs();
+        for (String arg : sourceArgs) {
+            System.out.print(arg + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/example/demo/FaceEngineService.java
@@ -0,0 +1,89 @@
+package com.example.demo;
+
+import com.arcsoft.face.*;
+import com.arcsoft.face.enums.DetectMode;
+import com.arcsoft.face.enums.DetectOrient;
+import com.arcsoft.face.enums.ErrorInfo;
+import com.arcsoft.face.toolkit.ImageInfo;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.arcsoft.face.toolkit.ImageFactory.getRGBData;
+@Service
+public  class FaceEngineService {
+
+    public static String appId = "6xLUZYEZvkMhJaphb5FBGhHNfqZDZUpBd5b7BuhV2pau";
+    public static String sdkKey = "ARe8YTRj66xwMFBjzSbcxYceRavZQYfCya9jDgdjetZX";
+    public static FaceEngine faceEngine = new FaceEngine("/root/cvLib");
+    public boolean startFaceEngine(){
+        System.out.println("start engine");
+        int errorCode = faceEngine.activeOnline(appId, sdkKey);
+        if (errorCode != ErrorInfo.MOK.getValue() && errorCode != ErrorInfo.MERR_ASF_ALREADY_ACTIVATED.getValue()) {
+            System.out.println("fail active engine1");
+        }
+        ActiveFileInfo activeFileInfo=new ActiveFileInfo();
+        errorCode = faceEngine.getActiveFileInfo(activeFileInfo);
+        if (errorCode != ErrorInfo.MOK.getValue() && errorCode != ErrorInfo.MERR_ASF_ALREADY_ACTIVATED.getValue()) {
+            System.out.println("fail active engine");
+        }
+        //引擎配置
+        EngineConfiguration engineConfiguration = new EngineConfiguration();
+        engineConfiguration.setDetectMode(DetectMode.ASF_DETECT_MODE_IMAGE);
+        engineConfiguration.setDetectFaceOrientPriority(DetectOrient.ASF_OP_ALL_OUT);
+        engineConfiguration.setDetectFaceMaxNum(10);
+        engineConfiguration.setDetectFaceScaleVal(16);
+        //功能配置
+        FunctionConfiguration functionConfiguration = new FunctionConfiguration();
+        functionConfiguration.setSupportAge(true);
+        functionConfiguration.setSupportFace3dAngle(true);
+        functionConfiguration.setSupportFaceDetect(true);
+        functionConfiguration.setSupportFaceRecognition(true);
+        functionConfiguration.setSupportGender(true);
+        functionConfiguration.setSupportLiveness(true);
+        functionConfiguration.setSupportIRLiveness(true);
+        engineConfiguration.setFunctionConfiguration(functionConfiguration);
+        //初始化引擎
+        errorCode = faceEngine.init(engineConfiguration);
+
+        if (errorCode != ErrorInfo.MOK.getValue()) {
+            System.out.println("fail init engine");
+        }
+        return true;
+    }
+    public byte[] getFeatureData(String _url) throws Exception {
+        URL url = new URL(_url);
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(5 * 1000);
+        InputStream inStream = conn.getInputStream();
+        byte[] data = readInputStream(inStream);
+        int errorCode = 0;
+        //人脸检测
+        ImageInfo imageInfo = getRGBData(data);
+        List<FaceInfo> faceInfoList = new ArrayList<FaceInfo>();
+        errorCode = faceEngine.detectFaces(imageInfo.getImageData(), imageInfo.getWidth(), imageInfo.getHeight(), imageInfo.getImageFormat(), faceInfoList);
+        //特征提取
+        FaceFeature faceFeature = new FaceFeature();
+        errorCode = faceEngine.extractFaceFeature(imageInfo.getImageData(), imageInfo.getWidth(), imageInfo.getHeight(), imageInfo.getImageFormat(), faceInfoList.get(0), faceFeature);
+        System.out.println("feature size：" + faceFeature.getFeatureData().length);
+        return faceFeature.getFeatureData();
+    }
+
+    public  byte[] readInputStream(InputStream inStream) throws Exception{
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while( (len=inStream.read(buffer)) != -1 ){
+            outStream.write(buffer, 0, len);
+        }
+        inStream.close();
+        return outStream.toByteArray();
+    }
+}
